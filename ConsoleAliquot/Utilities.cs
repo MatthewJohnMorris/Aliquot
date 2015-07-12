34ae74e6d6a56500d720a0388edcce791eaf59ab
MatthewJohnMorris <matt_morris_zz@yahoo.co.uk>
2015-07-12 18:34:43 +0100

    Add 5 parameter override to Log and LogLine

--- ConsoleAliquot/Utilities.cs
+++ ConsoleAliquot/Utilities.cs
@@ -11,11 +11,13 @@ namespace ConsoleAliquot
     public static void Log(string s, object o1, object o2) { Trace.Write(string.Format(s, o1, o2)); }
     public static void Log(string s, object o1, object o2, object o3) { Trace.Write(string.Format(s, o1, o2, o3)); }
     public static void Log(string s, object o1, object o2, object o3, object o4) { Trace.Write(string.Format(s, o1, o2, o3, o4)); }
+    public static void Log(string s, object o1, object o2, object o3, object o4, object o5) { Trace.Write(string.Format(s, o1, o2, o3, o4, o5)); }
     public static void LogLine(string s) { Trace.WriteLine(s); }
     public static void LogLine(string s, object o1) { Trace.WriteLine(string.Format(s, o1)); }
     public static void LogLine(string s, object o1, object o2) { Trace.WriteLine(string.Format(s, o1, o2)); }
     public static void LogLine(string s, object o1, object o2, object o3) { Trace.WriteLine(string.Format(s, o1, o2, o3)); }
     public static void LogLine(string s, object o1, object o2, object o3, object o4) { Trace.WriteLine(string.Format(s, o1, o2, o3, o4)); }
+    public static void LogLine(string s, object o1, object o2, object o3, object o4, object o5) { Trace.WriteLine(string.Format(s, o1, o2, o3, o4, o5)); }
 
     public delegate void OutputToBinaryWriter(BinaryWriter writer);
 
