f3ec7639b488ee564080fe9526f2e288c457cf7e
MatthewJohnMorris <matt_morris_zz@yahoo.co.uk>
2015-07-15 11:40:14 +0100

    Make class static

--- Aliquot.Common/Utilities.cs
+++ Aliquot.Common/Utilities.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 
 namespace Aliquot.Common
 {
-  internal class Utilities
+  internal static class Utilities
   {
     public static void Log(string s) { Trace.Write(s); }
     public static void Log(string s, object o1) { Trace.Write(string.Format(s, o1)); }
